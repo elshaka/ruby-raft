edddbdfd1efff603e5e1f97d6d243ef4b2e3331e
Eleazar Meza <meza.eleazar@gmail.com>
2024-10-18 20:04:28 -0400

    Implemented basic message functionality using Ractor

--- lib/node.rb
+++ lib/node.rb
@@ -3,9 +3,52 @@
 require 'securerandom'
 
 class Node
-  attr_reader :id
+  def initialize(name)
+    @name = name
+    @uuid = SecureRandom.uuid
+    @neighbors = []
+    init_log
+    init_actor
+  end
 
-  def initialize
-    @id = SecureRandom.uuid
+  def add_neighbor(node)
+    @neighbors << node unless @neighbors.include?(node)
+  end
+
+  def propose_state(state); end
+
+  def simulate_partition(partition); end
+
+  def retrieve_log
+    @log
+  end
+
+  def hello
+    @neighbors.each do |node|
+      node.send @name, 'hello'
+    end
+  end
+
+  def send(sender, message)
+    @actor << [sender, message]
+  end
+
+  def take
+    @actor.take
+  end
+
+  private
+
+  def init_log
+    @log = [{ index: 0, state: nil }]
+  end
+
+  def init_actor
+    @actor = Ractor.new name: @name do
+      loop do
+        sender, message = Ractor.receive
+        puts "#{sender} sent #{message} to #{name}"
+      end
+    end
   end
 end
--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -4,8 +4,4 @@ require 'spec_helper'
 require './lib/node'
 
 describe Node do
-  it 'initializes with a unique id' do
-    nodes_ids = 10.times.map { Node.new.id }
-    expect(nodes_ids.uniq).to eq(nodes_ids)
-  end
 end
